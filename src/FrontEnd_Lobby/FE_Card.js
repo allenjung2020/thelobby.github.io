76ed547a9c656248420f5fe03afa8c502486b5c0
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-03-27 17:44:20 -0700

    card height constrained

--- src/FrontEnd_Lobby/FE_Card.js
+++ src/FrontEnd_Lobby/FE_Card.js
@@ -14,18 +14,28 @@ class FE_Card extends Component {
     
     constructor(props) {
     	super(props);
+    	this.state = {
+    		class: "hide"
+    	};
     }
 
+    clickedToShow = () => {
+    	if (this.state.class === "hide") {
+    		this.setState({class: "show"});
+    	} else {
+    		this.setState({class: "hide"});
+    	}
+    }
 
     // to use images without import use require("path")
     // need to use a transparent div to align thing to the top
 	render() {
 	    return(
-	        <div className="FE_Card">
+	        <div className={"FE_Card " + this.state.class}>
 	        	<div style={{color: "transparent"}}>_</div>
-	        	<img src={ require("" + this.props.img)} alt="company profile image" />
+	        	<img src={ require("" + this.props.img)} alt="company profile" />
 	        	<h1>{this.props.subject}</h1>
-	        	<p>{this.props.description}</p>
+	        	<p  onClick={e => this.clickedToShow()} >{this.props.description}</p>
 	        	<p>{this.props.number + " in line"}</p>
 	        	<button>Join Queue</button>
 	        </div>
