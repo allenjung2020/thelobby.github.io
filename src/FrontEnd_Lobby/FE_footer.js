07da11752d9422d750fefcfbb7c3a5797a48f1d6
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-03-27 21:06:24 -0700

    + feedback box

--- src/FrontEnd_Lobby/FE_footer.js
+++ src/FrontEnd_Lobby/FE_footer.js
@@ -1,16 +1,34 @@
 import React, {Component} from 'react';
-
+import "./FE_footer.css"
 
 class FE_footer extends Component {
     
     constructor(props) {
     	super(props);
+    	this.state = {
+    		textareaClassName: "hideInput"
+    	};
+    }
+
+    showHideInput = () => {
+    	if (this.state.textareaClassName === "hideInput") {
+    		this.setState({textareaClassName: "showInput"});
+    	} else {
+    		this.setState({textareaClassName: "hideInput"});
+    	}
     }
 
 	render() {
 	    return(
-	        <div class="FE_footer">
-	        	
+	        <div className="FE_footer">
+	        	Footer
+	        	<div className="feedback">
+	        		<p onClick={e => this.showHideInput()}>Bugs? Feedbacks? Suggestions? Tell us here</p>
+	        		<div className="inputDiv">
+	        			<textarea className={this.state.textareaClassName} />
+	        			<button>Send</button>
+	        		</div>
+	        	</div>
 	        </div>
 	    );
 	}
