bd24d0a638e8fe51cc1a5f6cfe13438769807716
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-03-31 21:25:31 -0700

    hide backend to top left

--- src/NaviBar.js
+++ src/NaviBar.js
@@ -3,23 +3,10 @@ import {Link} from "react-router-dom";
 
 function NaviBar() {
     return(
-        <p>
-            {/*<div className="Navigation">*/}
-            {/*    <Link to="/Home">Home</Link>*/}
-            {/*</div>*/}
-            <div className="Navigation">
-                <Link to="/">FrontEnd</Link>
-            </div>
-            <div className="Navigation">
-                <Link to="/BackEnd_Company/BackEnd">BackEnd</Link>
-            </div>
-        </p>
-        // <ul>
-        //     <li><Link to="/Home">Home</Link></li>
-        //     <li><Link to="/FrontEnd_Lobby/FrontEnd">FrontEnd</Link></li>
-        //     <li><Link to="/BackEnd_Company/BackEnd">BackEnd</Link></li>
-        //     {/*<li><Link to="/">Home</Link></li>*/}
-        // </ul>
+        <div className="Navigation">
+            <Link to="/">FrontPage</Link>
+            <Link to="/BackEnd_Company/BackEnd">Backpage</Link>
+        </div>
     );
 }
 
