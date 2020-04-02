2c087b78a2b513de3f73eebbc5a25703694cc2c8
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-04-02 16:34:31 -0700

    remove route

--- src/App.js
+++ src/App.js
@@ -1,30 +1,50 @@
-import React from 'react';
-// import logo from './logo.svg';
+// import React, {Component} from 'react';
+import React, {Component} from 'react';
+
 import './App.css';
 import {BrowserRouter as Router, Route, Link} from 'react-router-dom';
 
 import NaviBar from "./NaviBar";
-import "./NaviBar.css";
 
-// import Home from "./Home";
 import BackEnd from "./BackEnd_Company/BackEnd";
 import FrontEnd from "./FrontEnd_Lobby/FrontEnd";
-          // <Route exact path="/Home" component={Home}/>
-function App() {
-  return (
-    <div className="App">
-        {/*<Route exact path="/" />*/}
-        <Router>
-          <NaviBar/>
-          {/*<nav>*/}
-          {/*  /!*<Link to={}*!/*/}
-          {/*</nav>*/}
-          <Route exact path="/" component={FrontEnd}/>
-          <Route exact path="/BackEnd_Company/BackEnd" component={BackEnd}/>
-          {/*<NaviBar/>*/}
-        </Router>
-    </div>
-  );
+        // <Route exact path="/BackEnd" component={BackEnd} />
+        // <Route exact path="/" component={FrontEnd} />
+class App extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: "front"
+    };
+  }
+
+  createHtml() {
+    if (this.state.page === "front") {
+      return <FrontEnd/>;
+    } else {
+      return <BackEnd/>;
+    }
+  }
+
+  switchPage = () => {
+    if (this.state.page === "front") {
+      this.setState({page: "back"});
+    } else {
+      this.setState({page: "front"});
+    }
+  }
+
+  render() {
+    return (
+      <div className="App">
+          {console.log("Only front end is built, backend features not accessable")}
+          {this.createHtml()}
+          <button style={{position:"fixed", top:0, left:0}} onClick={this.switchPage}>Switch</button>
+      </div>
+    );
+
+  }
 }
 
 export default App;
