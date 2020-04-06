b569d0fd2bdd6b182a4eef6ff2676c551ecafef8
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-04-06 00:13:06 -0700

    Preparing files for tutoring

---
+++ src/FrontEnd_Lobby/ChatNTutor/TutorLobby.js
@@ -0,0 +1,71 @@
+import React, {Component} from 'react';
+import "./TutorLobby.css";
+
+
+
+
+/**
+ * The lobby area for tutoring that stores
+ * all the current subjects (cards), active
+ * users/tutors (dots) of that subject.
+ *
+ * The default chatroom will be open to everyone and 
+ * be the default background of the lobby
+ *
+ * After entering and exiting a specific chatroom, that 
+ * room will be the background, blured
+ *
+ * Entering chatroom animation: carding flying to the left
+ * Button to exit "chatroom" 
+ * 
+ * Chatroom will enforce signing in
+ */
+
+/**
+ * hardcode in TA data
+ * so they are TA after "sign in"
+ */
+
+
+/**
+ * Cards
+ * On each subject card:
+ * 	Show 
+ * 		subject,
+ * 			description of level
+ * 	 	active tutors, professors?
+ * 	 	active students
+ */
+
+/**
+ * Background
+ * 	Input box,
+ * 	Conversation
+ * 		Name, message, time?
+ */
+
+/*
+	From props:
+		username
+ */
+class TutorLobby extends Component {
+    
+    constructor(props) {
+    	super(props);
+    	this.state = {
+    		channel = "default",
+    		lobby = true
+    	};
+    }
+
+
+	render() {
+	    return(
+	        <div id={"TutorLobby"}>
+	        	
+	        </div>
+	    );
+	}
+}
+
+export default TutorLobby;
