7dbcc138665a6116d044b0fc1ff32827d07bca99
Leo Liao(Home) <41461222+leol15@users.noreply.github.com>
2020-04-07 16:02:43 -0700

    change homepage back to home

--- src/FrontEnd_Lobby/FrontEnd.js
+++ src/FrontEnd_Lobby/FrontEnd.js
@@ -24,7 +24,7 @@ class FrontEnd extends Component {
     		userName: "",
     		email: "",
             buttonName: "Sign in",
-            body: "TutorLobby" //default page is home
+            body: "home" //default page is home
     	}
     }
 
